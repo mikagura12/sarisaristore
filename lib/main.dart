a7bc9e31fc5c24b7344b34ed390e9bc4ed3e67ef
Karl Jan <reginaldokarljan@gmail.com>
2021-02-17 23:08:20 +0800

    testing updating spec file

--- lib/main.dart
+++ lib/main.dart
@@ -23,6 +23,7 @@ class _MyAppState extends State<MyApp> {
     print('------------- Initializing -------------');
     sariStoreLocalDataSource = SariStoreLocalDataSource();
     sariSariStoreRepository = SariSariStoreRepository(sariStoreLocalDataSource);
+    
     super.initState();
   }
 
